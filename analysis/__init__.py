e40437613a2f183dbf381d1c38e66acd37211741
KyleLopin <kvl6@case.edu>
2023-10-25 18:59:33 +0700

    Made analysis files a directory for importing for tests

---
+++ analysis/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2023 Kyle Lopin (Naresuan University) <kylel@nu.ac.th>
+
+"""
+Make analysis folder a directory for easier importing for tests
+"""
+
+__author__ = "Kyle Vitautas Lopin"
