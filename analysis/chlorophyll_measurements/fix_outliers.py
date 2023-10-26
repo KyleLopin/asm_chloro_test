bcc332c011144d79cf52839af60842eeb48e8583
KyleLopin <kvl6@case.edu>
2023-10-26 08:31:17 +0700

    Added function to calculate the average leaf levels (used for chlorophyll here but generalized)

--- analysis/chlorophyll_measurements/fix_outliers.py
+++ analysis/chlorophyll_measurements/fix_outliers.py
@@ -36,7 +36,7 @@ def get_data(leaf: str) -> pd.DataFrame:
         "Mango", "Rice", or "Sugarcane"
 
     Returns:
-        pd.DataFrame of the chlorophyll measurements
+        pd.DataFrame: of the chlorophyll measurements
 
     """
     data_file = DATA_FOLDER / f"{leaf} Chlorophyll content.csv"
@@ -47,6 +47,58 @@ def get_data(leaf: str) -> pd.DataFrame:
         _data = _data.ffill()
         _data["Leaf No."] = _data["Leaf No."].astype(int)
     return _data
+
+
+def add_leave_average(_df: pd.DataFrame,
+                      column_values_to_average: str = 'Total Chlorophyll (µg/cm2)',
+                      column_to_groupby: str = "Leaf No.") -> pd.DataFrame:
+    """
+
+    Args:
+        _df (pd.DataFrame): DataFrame to make the average column in
+        column_values_to_average (str): name of the dataframe column of values to average
+        column_to_groupby (str): name of column to average by
+
+    Returns:
+        pd.DataFrame: new dataframe with averages added to a new column.
+
+        The new average column added with the name f"Avg {column_values_to_average}"
+        of the averages of column_values_to_average
+
+    Examples:
+        >>> df = pd.DataFrame({
+        ...      'Leaf No.': [1, 1, 2, 2],
+        ...      'Total Chlorophyll (µg/cm2)': [10, 20, 30, 40]
+        ... })
+        >>> add_leave_average(df)
+           Leaf No.  Total Chlorophyll (µg/cm2)  Avg Total Chlorophyll (µg/cm2)
+        0         1                          10                            15.0
+        1         1                          20                            15.0
+        2         2                          30                            35.0
+        3         2                          40                            35.0
+
+    """
+    # error checking the correct columns are included
+    if column_to_groupby not in _df.columns:
+        raise KeyError(f"'{column_to_groupby}' needs to be one of the columns in the dataframe")
+    if column_values_to_average not in _df.columns:
+        raise KeyError(f"Can not average '{column_values_to_average}' "
+                       f"as it is not in the dataframe")
+
+    # sometimes the Leaf number is only filled in for the first measurement not the
+    # other measurements, this wil fill in the
+    # column_to_groupby is in _df.columns because it was checked earlier
+    _new_df = _df.ffill()
+    _new_df[column_to_groupby] = _new_df[column_to_groupby].astype(int)
+
+    # average the column_value_to_average by the column_to_groupby value
+    for leaf in _new_df[column_to_groupby].unique():
+        # print(f"leaf: {leaf}")
+        leaf_avg = _new_df.loc[_new_df[column_to_groupby] == leaf][column_values_to_average].mean()
+        # print(leaf_avg)
+        _new_df.loc[_new_df[column_to_groupby] == leaf,
+                    f"Avg {column_values_to_average}"] = leaf_avg
+    return _new_df
 
 
 if __name__ == '__main__':
