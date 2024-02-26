031d2a8af3868775785f719099db4f4271d8be15
KyleLopin <kvl6@case.edu>
2024-02-26 19:02:33 +0700

    Added option to change the file ending to get the data from, ended up not even using this after implimentations, but can be used later.

--- analysis/chlorophyll_measurements/get_data.py
+++ analysis/chlorophyll_measurements/get_data.py
@@ -1,6 +1,20 @@
 # Copyright (c) 2023 Kyle Lopin (Naresuan University) <kylel@nu.ac.th>
 
 """
+Module for loading raw chlorophyll data for a set of leaves.
+
+This module provides functions to load CSV files containing raw chlorophyll data
+for specific leaves. The function, get_data(), loads the data from the file,
+selecting specified columns and returning them as a pandas DataFrame.
+
+Standard Libraries:
+    pathlib.Path: Provides object-oriented interface for working with filesystem paths.
+
+Installed Libraries:
+    pandas: A powerful data manipulation library.
+
+Attributes:
+    DATA_FOLDER (pathlib.Path): Path to the folder containing collected chlorophyll data.
 
 """
 
@@ -15,8 +29,10 @@ import pandas as pd
 # DATA_FOLDER = Path.cwd().parent.parent / "data" / "chlorophyll_data" / "collected_data"
 DATA_FOLDER = Path(__file__).parent.parent.parent / "data" / "chlorophyll_data" / "collected_data"
 
+
 def get_data(leaf: str, use_columns: tuple[str] = ("Total Chlorophyll (µg/cm2)",
-                                                   "Spot", "Leaf No.")) -> pd.DataFrame:
+                                                   "Spot", "Leaf No."),
+             file_ending: str = " Chlorophyll content.csv") -> pd.DataFrame:
     """ Load csv file with raw chlorophyll data for a set of leaves.
 
     Find a csv file with the name f"{leaf} Chlorophyll content.csv" in the DATA_FOLDER
@@ -27,13 +43,15 @@ def get_data(leaf: str, use_columns: tuple[str] = ("Total Chlorophyll (µg/cm2)"
         "Mango", "Rice", or "Sugarcane"
         use_columns (tuple[str]): Name of the columns to get from the data file to return.
         Use a tuple to prevent the defaults from mutating.
+        file_ending (str): End of the file name to get the data from.
+        Defaults to " Chlorophyll content.csv"
 
     Returns:
         pd.DataFrame: DataFrame of the file contents with the column names passed in.
 
     """
     # set the data file path
-    data_file = DATA_FOLDER / f"{leaf} Chlorophyll content.csv"
+    data_file = DATA_FOLDER / f"{leaf}{file_ending}"
     # read the data file, convert the use_columns to list if they are a tuple from defaults
     _data = pd.read_csv(data_file, usecols=list(use_columns))
     if "Leaf No." in _data.columns:
