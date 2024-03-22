d20334ea1bbd41b680e8013d3f02ed53c1719bb3
KyleLopin <kvl6@case.edu>
2024-03-22 09:42:24 +0700

    made as7262 and as7263 graphs, needs docs

--- analysis/spectrum_fitting/visualize_raw_data.py
+++ analysis/spectrum_fitting/visualize_raw_data.py
@@ -22,8 +22,9 @@ plt.style.use('seaborn-v0_8-dark')
 COLOR_MAP = 'Greens'  # options Greens, YlGn
 
 
-def visualize_raw_data(sensor="as7262", leaf="banana",
-                       measurement_type="reflectance"):
+def visualize_raw_data(ax: plt.Axes = None, fig: plt.Figure = None,
+                       sensor: str = "as7262", leaf: str = "banana",
+                       measurement_type: str = "reflectance"):
     """ Make a graph to visualize basic data, use this to test what is best for paper
 
 
@@ -35,42 +36,64 @@ def visualize_raw_data(sensor="as7262", leaf="banana",
     Returns:
 
     """
+    if not ax:
+        fig, ax = plt.subplots(1, 1)
     data = get_data.get_data(sensor=sensor, leaf=leaf,
                              measurement_type=measurement_type,
                              mean=True)
-    print(data)
     int_time = 100
     current = "25 mA"
     data = data[data["integration time"] == int_time]
     data = data[data["led current"] == current]
-    print(data)
     x_columns = []
+    x = []
     for column in data.columns:
         if 'nm' in column:
             x_columns.append(column)
+            x.append(int(column.split()[0]))
     x_data = data[x_columns]
-    # x_data = x_data.mean()
+    print(x)
+
     # make color map
-    color_map = mpl.colormaps[COLOR_MAP]
+    # color_map = mpl.colormaps[COLOR_MAP]
     color_map = mpl.colors.LinearSegmentedColormap.from_list("", ["palegreen", "darkgreen"])
 
     colormap_min = data["Avg Total Chlorophyll (µg/cm2)"].min()
     colormap_max = data["Avg Total Chlorophyll (µg/cm2)"].max()
     map_norm = mpl.colors.Normalize(vmin=colormap_min,
                                     vmax=colormap_max)
-    print(color_map)
-
-    lines = plt.plot(x_data.T)
+    # print(color_map)
+    lines = ax.plot(x, x_data.T, alpha=0.7)
+    # cax = fig.add_axes([.9, 0.1, 0.05, 0.5])
     # set the color of each line according to its chlorophyll level
     for i, line in enumerate(lines):  # type: int, mpl.lines.Line2D
         line.set_color(color_map(map_norm(data["Avg Total Chlorophyll (µg/cm2)"]))[i])
+    # make mean line
+    x_mean = x_data.mean()
+    ax.plot(x, x_mean, color="black", label="mean")
     # scalar_map.set_clim([real_colormap_min, colormap_max])
     # add color bar to show chlorophyll level colors
-    plt.colorbar(mpl.cm.ScalarMappable(norm=map_norm, cmap=color_map),
-                 orientation="vertical", label="Total Chlorophyll (µg/cm2)",
-                 fraction=0.08)
-    plt.show()
+    # fig.colorbar(mpl.cm.ScalarMappable(norm=map_norm, cmap=color_map),
+    #              cax=cax, orientation="vertical",
+    #              label="Total Chlorophyll (µg/cm2)", fraction=0.08)
+
 
 
 if __name__ == '__main__':
-    visualize_raw_data()
+    figure, axs = plt.subplots(2, 2,
+                               sharex="col")
+    for i, sensor in enumerate(["as7262", "as7263"]):
+        print(f"i = {i}")
+        visualize_raw_data(ax=axs[0, i], fig=figure, sensor=sensor)
+        axs[0, i].set_title(f"{sensor.upper()}\nReflectance")
+        axs[0, i].set_ylabel("% reflectance")
+        visualize_raw_data(ax=axs[1, i], fig=figure,
+                           measurement_type="raw",
+                           sensor=sensor)
+        axs[1, i].set_title("Raw Measurements")
+        axs[1, i].set_xlabel("Wavelength (nm)")
+        axs[1, i].set_ylabel("Counts")
+
+
+    plt.tight_layout()
+    plt.show()
