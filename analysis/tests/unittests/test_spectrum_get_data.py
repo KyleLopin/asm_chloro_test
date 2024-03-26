482acccbfcb094b73d3498489b6201cc7b464017
KyleLopin <kvl6@case.edu>
2024-03-26 17:32:30 +0700

    linting

--- analysis/tests/unittests/test_spectrum_get_data.py
+++ analysis/tests/unittests/test_spectrum_get_data.py
@@ -10,7 +10,6 @@ __author__ = "Kyle Vitautas Lopin"
 import unittest
 
 # installed libraries
-import numpy as np
 import pandas as pd
 
 from analysis.spectrum_fitting import get_data
@@ -47,6 +46,7 @@ class TestGetOptions(unittest.TestCase):
 class TestGetData(unittest.TestCase):
     """ Test the get_data function"""
     def test_all_combination_size(self):
+        """ Test that the get_data.get_data function returns data of the correct shape """
         for _type in ["raw", "reflectance"]:
             for sensor in ["as7262", "as7263"]:
                 for leaf in ALL_LEAVES:
@@ -59,25 +59,33 @@ class TestGetData(unittest.TestCase):
 class TestGetXY(unittest.TestCase):
     """ Test the get_x_y function """
     def test_shapes(self):
+        """ Test that the get_data.get_x_y function returns the correct shape data, uses the
+            shape_test method to reduce nesting """
         for _type in ["raw", "reflectance"]:
             for sensor in ["as7262", "as7263"]:
                 for leaf in ALL_LEAVES:
-                    int_times, leds, currents = get_data.get_options(
-                        sensor=sensor, leaf=leaf,measurement_type=_type)
-                    for int_time in int_times:
-                        for led in leds:
-                            for current in currents:
-                                x, y = get_data.get_x_y(
-                                    sensor=sensor, leaf=leaf, measurement_type=_type,
-                                    int_time=int_time, led=led, led_current=current)
-                                self.assertTrue(x.shape[0] >= 299)
-                                self.assertTrue(x.shape[1] == 6)
-                                self.assertTrue(y.shape[0] >= 299)
-                                self.assertTrue(y.shape[1] == 6)
+                    self.shape_test(sensor, leaf, _type)
+
+    def shape_test(self, sensor, leaf, _type):
+        """ helper function for the main method test_shapes """
+        int_times, leds, currents = get_data.get_options(
+            sensor=sensor, leaf=leaf, measurement_type=_type)
+        for int_time in int_times:
+            for led in leds:
+                for current in currents:
+                    x, y = get_data.get_x_y(
+                        sensor=sensor, leaf=leaf, measurement_type=_type,
+                        int_time=int_time, led=led, led_current=current)
+                    self.assertTrue(x.shape[0] >= 299)
+                    self.assertTrue(x.shape[1] == 6)
+                    self.assertTrue(y.shape[0] >= 299)
+                    self.assertTrue(y.shape[1] == 6)
 
 
 class TestGetDataSlices(unittest.TestCase):
+    """ Test the get_data.get_data_slices function """
     def test_simple(self):
+        """ Simple test for development """
         df = pd.DataFrame({
             'Leaf No.': [1, 2, 3],
             'foo': [10, 20, 30],
@@ -92,7 +100,10 @@ class TestGetDataSlices(unittest.TestCase):
         })
         assert correct_df.equals(results)
 
-    def test_real_data_as7262(self):
+    def test_real_data(self):
+        """ Test that getting the real data returns the correct results by going through each
+         measurement type, sensor and leaf combination, uses the helper method each_condition
+         to do the actual asserting """
         for _type in ["raw", "reflectance"]:
             for sensor in ["as7262", "as7263", "as7265x"]:
                 for leaf in ALL_LEAVES:
@@ -101,6 +112,8 @@ class TestGetDataSlices(unittest.TestCase):
                     self.each_condition(data, leaf, sensor)
 
     def each_condition(self, data, leaf, sensor):
+        """ Take a DataFrame and go through each led, integration time and led current
+         and assert it has the correct shape """
         for led in data["led"].unique():
             for int_time in data["integration time"].unique():
                 for current in data["led current"].unique():
@@ -111,10 +124,12 @@ class TestGetDataSlices(unittest.TestCase):
                     result = get_data.get_data_slices(result, "led current",
                                                       [current])
                     # print(result.shape, (led, int_time, current, leaf, sensor))
-                    if (led, int_time, current, leaf, sensor) == ("White LED", 200, "50 mA", "banana", "as7262"):
+                    if ((led, int_time, current, leaf, sensor) ==
+                            ("White LED", 200, "50 mA", "banana", "as7262")):
+                        # this read has 1 that was saturated so is 1 smaller than normal
                         self.assertTrue(result.shape == (299, 20))
-                    elif sensor == "as7265x":
-                        if current != "100 mA":
+                    elif sensor == "as7265x":  # this has a different shape because of more channels
+                        if current != "100 mA":  # not all 100 mA conditions were tested so skip it
                             self.assertTrue(result.shape == (300, 32))
                     else:
                         self.assertTrue(result.shape == (300, 20))
