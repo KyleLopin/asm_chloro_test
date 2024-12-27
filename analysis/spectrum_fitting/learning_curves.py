d1a18736bbe1e4e94e6350452218e44c7cde2dae
KyleLopin <kvl6@case.edu>
2024-12-27 15:49:47 +0700

    cleaned up some depricated parts, added savefig and annotations for panel labels

--- analysis/spectrum_fitting/learning_curves.py
+++ analysis/spectrum_fitting/learning_curves.py
@@ -6,84 +6,57 @@
 
 __author__ = "Kyle Vitautas Lopin"
 
+# standard libraries
+from cycler import cycler
 
 # installed libraries
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
 import numpy as np
 import pandas as pd
-from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.base import BaseEstimator
 from sklearn.cross_decomposition import PLSRegression
-from sklearn.decomposition import KernelPCA
-from sklearn.ensemble import BaggingRegressor, GradientBoostingRegressor, StackingRegressor
-from sklearn.linear_model import (ARDRegression, HuberRegressor,
-                                  LinearRegression, LassoLarsIC)
-from sklearn.model_selection import GroupShuffleSplit
-from sklearn.model_selection import cross_validate, LearningCurveDisplay
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import (FunctionTransformer,
-                                   PolynomialFeatures, StandardScaler)
-plt.style.use("ggplot")
+from sklearn.model_selection import GroupShuffleSplit, LearningCurveDisplay
+from sklearn.preprocessing import (StandardScaler)
+# plt.style.use("ggplot")
 
 # local files
 import get_data
-from global_classes import CVScores
-import preprocessors
-from remove_outliers import remove_outliers_from_residues
+from stratified_group_shuffle_split import StratifiedGroupShuffleSplit
 
-CV = GroupShuffleSplit(test_size=0.2, n_splits=10)
-CV = GroupShuffleSplit(test_size=0.2, n_splits=100)
 
-TRAINING_SIZES = [0.5, 0.75, 0.9, 1.0]
+YLIM = ([0, -8])
+TRAINING_SIZES = [0.1, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+TEST_SIZE = 0.2
 SCORING = "r2"
+SCORING = "neg_mean_absolute_error"
 LINESTYLES = ['-', '--', ':', '-', '-', ':', '-', '-', ':']
 COLORS = ['black', 'blue', 'red', "green", 'magenta', 'cyan', 'darkred',
           'cyan', 'gold']
 LEAF = "banana"
+SENSORS = ["as7262", "as7263", "as7265x"]
 ALL_LEAVES = ["mango", "banana", "jasmine", "rice", "sugarcane"]
-MEASUREMENT_TYPES = ["raw", "reflectance", "absorbance"]
 
-polynomial = FunctionTransformer(preprocessors.polynomial_expansion)
+pls_n_comps = {"as7262": {"banana": 6, "jasmine": 4, "mango": 5, "rice": 6, "sugarcane": 6},
+               "as7263": {"banana": 5, "jasmine": 5, "mango": 5, "rice": 5, "sugarcane": 5},
+               "as7265x": {"banana": 8, "jasmine": 14, "mango": 11, "rice": 6, "sugarcane": 6}}
+
+CV = GroupShuffleSplit(test_size=TEST_SIZE, n_splits=10)
+N_SPLITS = 200  # change to make faster
+CV = StratifiedGroupShuffleSplit(test_size=TEST_SIZE, n_splits=N_SPLITS,
+                                 n_bins=10, random_state=128)
+# CV = ShuffleSplit(test_size=TEST_SIZE, n_splits=N_SPLITS)
 
 
-pipe2 = make_pipeline(KernelPCA(n_components=20, kernel="rbf"),
-                      LassoLarsIC(criterion='bic'))
-
-
-grad_boost_depth=3
-regressors = {
-    "Linear Regression": LinearRegression(),
-
-              "PLS": PLSRegression(n_components=10),
-              # "Poly PLS (20 comps)": PLSRegression(n_components=20),
-
-              "LassoCV BIC": LassoLarsIC(criterion='bic'),
-              "Gradient Boost 50": GradientBoostingRegressor(n_estimators=50),
-              "ARD": ARDRegression(lambda_2=0.001),
-              "Hubur": HuberRegressor(max_iter=10000),
-              # "Stacked ARD Gradient": StackingRegressor(
-              #     [("ard", ARDRegression(lambda_2=0.001)),
-              #      ("gradient", GradientBoostingRegressor(max_depth=2))]),
-              # "Stacked Hubur Gradient": StackingRegressor(
-              #     [("gradient", GradientBoostingRegressor(max_depth=2)),
-              #      ("Hubur", HuberRegressor(max_iter=10000))]),
-              # "Stacked ARD Hubur Gradient": StackingRegressor(
-              #     [("ard", ARDRegression(lambda_2=0.001)),
-              #      ("gradient", GradientBoostingRegressor(max_depth=2)),
-              #      ("Hubur", HuberRegressor(max_iter=10000))]),
-              # "Full stack": StackingRegressor(
-              #     [("ard", ARDRegression(lambda_2=0.001)),
-              #      ("gradient", GradientBoostingRegressor(n_estimators=50)),
-              #      ("Hubur", HuberRegressor(max_iter=10000)),
-              #      ("pls", PLSRegression(n_components=20, max_iter=10000))])
-              }
-# regressors = {"Linear Regression": LinearRegression()}
+def set_style():
+    default_colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    plt.style.use('ggplot')
+    plt.rcParams['axes.prop_cycle'] = cycler(color=default_colors)
 
 
 def make_learning_curves(ax: plt.Axes, x: pd.DataFrame,
                          y: pd.Series, groups: pd.Series, i: int,
                          name: str,
-                         regr: BaseEstimator = LinearRegression(),
+                         regr: BaseEstimator,
                          score: str = SCORING):
     LearningCurveDisplay.from_estimator(regr, x, y, groups=groups,
                                         cv=CV, scoring=score, n_jobs=-1,
@@ -94,103 +67,144 @@ def make_learning_curves(ax: plt.Axes, x: pd.DataFrame,
     ax.plot([], [], label=name, color=COLORS[i], linestyle=LINESTYLES[i])
 
 
-def find_number_pls_latent_variables(max_comps: int = 6, poly_degrees: int = 1):
-    _x, _y, _groups = get_data.get_x_y(sensor="as7265x", leaf="mango",
-                                       measurement_type="raw",
-                                       int_time=50,
-                                       send_leaf_numbers=True)
-    _y = _y["Avg Total Chlorophyll (µg/cm2)"]
-    cv = GroupShuffleSplit(test_size=0.2, n_splits=100)
-    cv_scores = CVScores()
-    if poly_degrees > 1:
-        print("_x shape: ", _x.shape, poly_degrees)
-        _x = PolynomialFeatures(degree=poly_degrees,
-                                include_bias=False
-                                ).fit_transform(_x)
-        print("_x shape: ", _x.shape)
-    for i in range(1, max_comps+1):
-        pls = PLSRegression(n_components=i, max_iter=5000)
-        scores = cross_validate(pls, _x, _y, groups=_groups,
-                                scoring='r2', cv=cv, return_train_score=True)
-        cv_scores.add_scores(i, scores)
-    cv_scores.plot()
-    plt.show()
-
-
-def make_learning_curves_graph(leaf: str = LEAF, remove_outliers=False,
-                               ax: plt.Axes|None=None, title: str="",
-                               measurement_type: str="raw", sensor: str="as7262",
-                               int_time: int=50, fit_poly: bool=True,
-                               show_legend=True):
-    if not ax:
-        fig, ax = plt.subplots()
-    if title:
-        ax.set_title(title+" "+sensor)
-    for i, (name, regressor) in enumerate(regressors.items()):
-        print(name)
-        if name == "PLS" and sensor in ['as7262', 'as7263']:
-            regressor = PLSRegression(n_components=10)
-        elif name == 'PLS':
-            regressor = PLSRegression(n_components=20)
-        _x, _y, _groups = get_data.get_x_y(sensor=sensor, led="White LED",
-                                           leaf=leaf,
-                                           measurement_type=measurement_type,
-                                           int_time=int_time,
-                                           send_leaf_numbers=True)
-        _y = _y["Avg Total Chlorophyll (µg/cm2)"]
-        if remove_outliers == "residues":
-            _x, _y, _groups = remove_outliers_from_residues(_x, _y, _groups)
-        if fit_poly:
-            _x = PolynomialFeatures(degree=2).fit_transform(_x)
-        _x = StandardScaler().fit_transform(_x)
-        make_learning_curves(ax, x=_x, y=_y, groups=_groups,
-                             i=i, name=name, regr=regressor)
-
-    if show_legend:
-        handles, labels = ax.get_legend_handles_labels()
-        # Filter out "Train" and "Test" entries
-        filtered_handles = [handle for handle, label in zip(handles, labels) if
-                            label not in ["Train", "Test"]]
-        filtered_labels = [label for label in labels if label not in ["Train", "Test"]]
-        plt.legend(filtered_handles, filtered_labels)
-
-
 def make_all_leaf_learning_curves():
-    for leaf in ALL_LEAVES:
-        make_learning_curves_graph(leaf)
+    set_style()
+    figure, axes = plt.subplots(5, 3, figsize=(7, 8.5),
+                                sharex=True, sharey=True)
 
+    for row, leaf in enumerate(["banana", "jasmine", "mango", "rice", "sugarcane"]):
+        for column, sensor in enumerate(SENSORS):
+            print(leaf, sensor, row, column)
+            # get the data
+            x, y, groups = get_data.get_cleaned_data(sensor, leaf,
+                                                     mean=False)
+            y = y['Avg Total Chlorophyll (µg/cm2)']
+            x = StandardScaler().fit_transform(x)
+            # get the number of components optimized for sensor / leaf combination
+            n_components = pls_n_comps[sensor][leaf]
+            regressor = PLSRegression(n_components=n_components)
+            LearningCurveDisplay.from_estimator(
+                regressor, x, y, groups=groups, cv=CV, scoring=SCORING,
+                n_jobs=-1, train_sizes=TRAINING_SIZES,
+                ax=axes[row][column])
+            axes[row][column].set_xlabel("")
+            axes[row][column].set_ylabel("")
+            if row != 2 or column != 1:
+                axes[row][column].legend().remove()
+            if row == 0:
+                axes[row][column].set_title("AS" + sensor[2:], fontsize=10)
+
+            # if SCORING == 'neg_mean_absolute_error':
+                # display.train_scores = -display.train_scores
+                # display.test_scores = -display.test_scores
+                # Plot with adjusted values
+                # display.plot(ax=axes[row][column])
+                # axes[row][column].plot(-display.train_scores, -display.test_scores)
+            if row == 4:
+                # convert TRAINING_SIZES to number of leaves
+                num_leaves = [x * 300 * (1 - TEST_SIZE) for x in TRAINING_SIZES]
+                # Calculate the labels, skipping half of them
+                labels = [int(size / 3) if idx % 2 == 0
+                          else "" for idx, size in enumerate(num_leaves)]
+
+                axes[row][column].set_xticks(ticks=num_leaves,
+                                             labels=labels)
+                if column == 1:  # Update x-axis label
+                    axes[row][column].set_xlabel("Number of Leaves in Training Set")
+
+
+            if row == 2 and column == 0:
+                axes[row][column].set_ylabel(
+                    "Mean Absolute Error of Total Chlorophyll ($\mu$g/cm$^2$)")
+            # convert the negative mean absolute to positive by reversing the y-axis
+            axes[row][column].set_ylim(YLIM)
+
+        # convert the negative mean absolute to positive
+        axes[row][0].set_yticks([0, -2, -4, -6], [0, 2, 4, 6])
+
+        axes[row][0].annotate(f"{leaf.capitalize()}", (0.5, 0.1),
+                              xycoords='axes fraction', fontsize=10)
+    # add annotations for the panels
+    i = 0
+    for column, sensor in enumerate(SENSORS):
+        for row, leaf in enumerate(["banana", "jasmine", "mango", "rice", "sugarcane"]):
+            axes[row][column].annotate(f"{chr(i + 97)})", (0.10, 0.05),
+                                       fontsize=10, fontweight='bold',
+                                       xycoords='axes fraction')
+            i += 1
+
+    figure.suptitle("Validation curves", fontsize=14)
+    plt.subplots_adjust(left=0.08, bottom=0.1, right=0.97, top=0.92, wspace=0.08)
+    figure.savefig("MEA_Validation_Curves.png", dpi=600)
     plt.show()
 
 
-def make_learning_curves_3_measure_modes(leaf:str, sensor: str="as7262",
-                                         save_fig:bool=False):
-    fig, axes = plt.subplots(1, 3, figsize=(12, 4),
-                             sharey=True)
-    show_legend=False
-    fig.suptitle(leaf)
-    for i, measurement_type in enumerate(MEASUREMENT_TYPES):
-        if i == 2:
-            show_legend = True
+def make_learning_curves_3_sensors(leaf: str):
+    """
+    Generate learning curve plots for three measurement modes of a specified leaf and sensor.
+    Used to compare which measurement mode is best to fit the model
 
-        make_learning_curves_graph(leaf, ax=axes[i],
-                                   sensor=sensor,
-                                   measurement_type=measurement_type,
-                                   title=measurement_type,
-                                   show_legend=show_legend)
+    Parameters
+    ----------
+    leaf : str
+        Name of the leaf type to analyze (e.g., 'mango', 'banana').
+
+    Returns
+    -------
+    None
+        This function generates and displays a figure containing three subplots,
+        each representing a learning curve for a different measurement mode.
+    """
+
+    default_colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    plt.style.use('ggplot')
+    plt.rcParams['axes.prop_cycle'] = cycler(color=default_colors)
+    fig, axes = plt.subplots(1, 3, figsize=(7.5, 3),
+                             sharey=True)
+    fig.suptitle(f"{leaf.capitalize()} Leaf Validation Curves", y=0.95)
+    for i, sensor in enumerate(SENSORS):
+        print(f'sensor: {sensor}')
+        x, y, groups = get_data.get_cleaned_data(sensor, leaf,
+                                                 mean=False)
+        y = y['Avg Total Chlorophyll (µg/cm2)']
+        x = StandardScaler().fit_transform(x)
+
+        n_components = pls_n_comps[sensor][leaf]
+        regressor = PLSRegression(n_components=n_components)
+
+        x = np.array(x, copy=True)
+        y = np.array(y, copy=True)
+        LearningCurveDisplay.from_estimator(
+            regressor, x, y, groups=groups, cv=CV, scoring=SCORING,
+            train_sizes=TRAINING_SIZES,
+            std_display_style="fill_between", ax=axes[i])
+        if i < 2:
+            axes[i].get_legend().remove()
+        if i > 0:
+            axes[i].set_ylabel("")  # Remove the ylabel
+        else:
+            axes[i].set_ylabel("$R^2$")
+        axes[i].set_ylim(YLIM)
+
+        # Update x-axis ticks and labels
+        # convert TRAINING_SIZES to number of leaves
+        num_leaves = [x*300*(1-TEST_SIZE) for x in TRAINING_SIZES]
+        # Calculate the labels, skipping half of them
+        labels = [int(size / 3) if idx % 2 == 0 else "" for idx, size in enumerate(num_leaves)]
+
+        axes[i].set_xticks(ticks=num_leaves,
+                           labels=labels)
+        axes[i].set_xlabel("Number of Leaves in Training Set")  # Update x-axis label
+
     axes[0].set_xlabel("")
     axes[2].set_xlabel("")
-    plt.tight_layout()
+    axes[2].legend()
+    plt.subplots_adjust(left=0.08, bottom=0.17, right=0.95)
+    fig.savefig(f"{leaf}_validation_curves.pdf", format='pdf')
+    # plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
-    # make_learning_curves_graph("banana")
-    with PdfPages('all learning curves.pdf') as pdf:
-        for sensor in ["as7262", "as7263", "as7265x"]:
-            for leaf in ALL_LEAVES:
-
-                make_learning_curves_3_measure_modes(leaf, sensor=sensor)
-                pdf.savefig()
-
-    # make_all_leaf_learning_curves()
-
-    # find_number_pls_latent_variables(poly_degrees=2, max_comps=25)
+    make_all_leaf_learning_curves()
+    # make_learning_curves_3_sensors("rice")
